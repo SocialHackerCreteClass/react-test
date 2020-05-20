823f4080c6e268ae5766dc5cf4032e96c43d2dc9
SalvadorCab <44257545+SalvadorCab@users.noreply.github.com>
2020-05-20 04:22:05 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <div className="App">
         <Switch>
           <Route path='/' exact component={Users} /> 
-          <Route path='*' component={Error}/>
         </Switch>
       </div>
     </Router>
